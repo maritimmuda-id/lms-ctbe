e2fb2fc37c75f502117ed8915b81fa153a2d5308
MDarmaWijaya <wijayamdarma0@gmail.com>
2024-05-16 15:54:29 +0700

    UI Login Fix

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,14 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
- 
-export default NextAuth(authConfig).auth;
- 
-export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-};
+// import NextAuth from 'next-auth';
+// import { authConfig } from './auth.config';
 
+// export default NextAuth(authConfig).auth;
 
-// import { NextResponse } from "next/server";     
+// export const config = {
+//   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+//   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+// };
+
+// import { NextResponse } from "next/server";
 // import type { NextRequest } from "next/server";
 
 // export function middleware(request: NextRequest) {
@@ -23,4 +22,3 @@ export const config = {
 // export const config = {
 //     matcher: ["/", "/dashboard", "/register", "/api/:path*"],
 // };
-
